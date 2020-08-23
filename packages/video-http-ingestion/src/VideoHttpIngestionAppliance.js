6c2c95c5ca4432d2884f237cbaba366f9ebfe350
Dan Schultz <slifty@gmail.com>
2020-08-23 18:15:16 -0400

    Add video-http-ingestion appliance
    The @tvkitchen/appliance-video-http-ingestion package will take a given
    video URL and convert it to STREAM.CONTAINER payloads.
    
    Issue #32
    
    Co-authored-by: Chris Zubak-Skees <chriszs@gmail.com>

---
+++ packages/video-http-ingestion/src/VideoHttpIngestionAppliance.js
@@ -0,0 +1,47 @@
+import { PassThrough } from 'stream'
+import fetch from 'node-fetch'
+import { AbstractVideoIngestionAppliance } from '@tvkitchen/appliance-core'
+
+/**
+ * The VideoHttpIngestionAppliance handles processing a HTTP video stream. It is a concrete
+ * implementation of AbstractIngestionAppliance, which reads in a stream, converts it into
+ * an MPEG-TS stream represented as a series of Payloads, and then emits those Payloads.
+ *
+ * @extends AbstractVideoIngestionAppliance
+ */
+class VideoHttpIngestionAppliance extends AbstractVideoIngestionAppliance {
+	/**
+	 * Create a VideoHttpIngestionEngine.
+	 *
+	 * @param  {string} settings.url The url of the stream to be ingested
+	 */
+	constructor(settings = {
+		url: '',
+	}) {
+		super(settings)
+		if (!settings.url) {
+			throw new Error(
+				'HttpIngestionAppliance must be instantiated with a stream URL',
+			)
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	getInputStream = () => {
+		const stream = new PassThrough()
+
+		fetch(this.settings.url)
+			.then((response) => {
+				response.body.pipe(stream)
+			})
+			.catch((err) => {
+				stream.emit('error', err)
+			})
+
+		return stream
+	}
+}
+
+export default VideoHttpIngestionAppliance
